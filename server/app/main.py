39cd4bba0c1d6acaeba334438b93da84d2be3b23
Faisal Alquaddoomi <faisal.alquaddoomi@cuanschutz.edu>
2022-02-02 13:15:29 -0700

    Adds duration tracking to /neighbors endpoint.

--- server/app/main.py
+++ server/app/main.py
@@ -1,6 +1,7 @@
 import logging
 from typing import Optional
 from fastapi import FastAPI
+from .tracking import ExecTimer
 
 from .neighbors import cutoff_points, extract_frequencies, extract_neighbors
 
@@ -17,24 +18,26 @@ def read_root():
 
 @app.get("/neighbors")
 def neighbors(tok: str):
-    # Extract the frequencies
-    frequency_output = extract_frequencies(tok)
-    logger.info("finished extract_frequencies()...")
+    with ExecTimer() as timer:
+        # Extract the frequencies
+        frequency_output = extract_frequencies(tok)
+        logger.info("finished extract_frequencies()...")
 
-    # Extract Estimated Cutoff Points
-    changepoint_output = cutoff_points(tok)
-    logger.info("finished cutoff_points()...")
+        # Extract Estimated Cutoff Points
+        changepoint_output = cutoff_points(tok)
+        logger.info("finished cutoff_points()...")
 
-    # Extract the neighbors
-    word_neighbor_map = extract_neighbors(tok)
-    logger.info("finished word_neighbor_map()...")
+        # Extract the neighbors
+        word_neighbor_map = extract_neighbors(tok)
+        logger.info("finished word_neighbor_map()...")
 
-    # Final Return Object
-    # DN: This object doesn't contain the umap plot needed for visualization.
-    # On my todolist of things to get done.
+        # Final Return Object
+        # DN: This object doesn't contain the umap plot needed for visualization.
+        # On my todolist of things to get done.
 
-    return {
-        "neighbors": word_neighbor_map,
-        "frequency": frequency_output,
-        "changepoints": changepoint_output,
-    }
+        return {
+            "neighbors": word_neighbor_map,
+            "frequency": frequency_output,
+            "changepoints": changepoint_output,
+            "elapsed": timer.snapshot()
+        }
