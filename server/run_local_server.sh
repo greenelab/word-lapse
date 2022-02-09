e0f71b4ee267ba0753f26f5cca68851a32dccd73
Faisal Alquaddoomi <faisal.alquaddoomi@cuanschutz.edu>
2022-02-09 13:57:42 -0700

    run_local_server.sh now uses the instance_env file and builds the image by default.

--- server/run_local_server.sh
+++ server/run_local_server.sh
@@ -4,13 +4,12 @@ IMAGE=word-lapse-api
 TAG=latest
 
 (
-    [[ "${BUILD_IMAGE:-0}" -eq 0 ]] \
+    [[ "${BUILD_IMAGE:-1}" -eq 0 ]] \
         && echo "* Skipping build, using ${IMAGE}:${TAG}" \
         || docker build -t ${IMAGE}:${TAG} .
 ) && \
     docker run --name word-lapse-api --rm -it \
         -p 8080:80 -v $PWD/data:/app/data \
         -e USE_HTTPS=0 -e UPDATE_DATA=0 \
-        -e PARALLELIZE_QUERY=true \
-        -e PARALLEL_POOLS=20 \
+        --env-file=instance_env \
         ${IMAGE}:${TAG} "$@"
