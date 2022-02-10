6220a1f5206b492ee9f74b3db7aa7458cb6a22ef
Faisal Alquaddoomi <faisal.alquaddoomi@cuanschutz.edu>
2022-02-10 16:11:52 -0700

    Removed unused imports, added some docstrings and organizing comments.

--- server/backend/main.py
+++ server/backend/main.py
@@ -1,16 +1,15 @@
 import asyncio
 import logging
 import os
-from typing import Optional
+import re
+from itertools import islice
 
 import redis
 from fastapi import FastAPI, HTTPException
 from fastapi_redis_cache import FastApiRedisCache, cache
-from rq import Queue, Worker
+from rq import Queue
 
 from .config import get_config_values
-from .neighbors import cutoff_points, extract_frequencies, extract_neighbors
-from .tracking import ExecTimer
 
 logger = logging.getLogger(__name__)
 
@@ -21,6 +20,11 @@ redis_cache : FastApiRedisCache = None
 # populated in init_rq(), used in neighbors()
 queue : Queue = None
 
+
+# ========================================================================
+# === handlers that run on server boot
+# ========================================================================
+
 # @app.on_event('startup')
 # async def emit_config():
 #     from .config import emit_config
@@ -28,6 +32,9 @@ queue : Queue = None
 
 @app.on_event("startup")
 async def init_redis_cache():
+    # initializes redis_cache, a FastAPI-compatible cache decorator.
+    # the cached entries are persisted in redis, a fast in-memory store with
+    # occasional disk persistence.
     global redis_cache
     redis_cache = FastApiRedisCache()
     redis_cache.init(
@@ -36,14 +43,20 @@ async def init_redis_cache():
 
 @app.on_event("startup")
 async def init_rq():
+    # initialize rq, the redis queue.
+    # moves expensive tasks to a separate process where they won't block the API
     global queue
     r = redis.from_url(os.environ.get("REDIS_URL"))
     queue = Queue('w2v_queries', connection=r)
 
+
+# ========================================================================
+# === helper methods
+# ========================================================================
+
 async def enqueue_and_wait(func, *args, **kwargs):
     """
-    Helper method to pass 'func' with any extra args
-    to the w2v_queries queue.
+    Helper method to pass 'func' with any extra args to the w2v_queries queue.
     """
     job = queue.enqueue(func, *args, **kwargs)
 
@@ -60,8 +73,16 @@ async def enqueue_and_wait(func, *args, **kwargs):
         )
 
 
+# ========================================================================
+# === endpoints
+# ========================================================================
+
 @app.get("/")
-async def read_root():
+async def server_meta():
+    """
+    Returns metadata about the server, e.g. config variables, the
+    commit that was used to build the server, etc.
+    """
     return {
         "name": "Word Lapse API",
         "commit_sha": os.environ.get("COMMIT_SHA", "unspecified"),
@@ -70,6 +91,9 @@ async def read_root():
 
 @app.get("/ping")
 async def ping_workers():
+    """
+    Sends a ping to a worker, for debugging purposes.
+    """
     from .w2v_worker import ping
     return {
         'result': await enqueue_and_wait(ping, 'hello?')
@@ -78,14 +102,77 @@ async def ping_workers():
 @app.get("/neighbors")
 @cache()
 async def neighbors(tok: str):
+    """
+    Returns information about the token 'tok' over all the years in the dataset.
+
+    Note that this operation can take a long time (1 minute+) if the word is not
+    cached. If it's previously been cached and hasn't been evicted, response times
+    should be within 1 second.
+
+    The returned object is of the form
+    {
+        "neighbors": {<year:str>: [<token:str>, ...]},
+        "frequency": [{"year": <year:int>, "frequency": <frequency:float>}, ...],
+        "changepoints": [{"changepoint_idx": <year_range:str>}, ...],
+        "elapsed": <elapsed_ms:float>
+    }
+
+    'year_range' in 'changepoints' is the span of years in which the change
+    took place, e.g. 2019-2020.
+    'elapsed_ms' is the length of time the request took to formulate on
+    the server.
+    """
     from .w2v_worker import get_neighbors
     return await enqueue_and_wait(get_neighbors, tok=tok, job_timeout=800)
 
 @app.get("/neighbors/cached")
 async def neighbors_is_cached(tok: str):
+    """
+    Queries the cache for 'tok', returning the token in the 'token'
+    field and whether it's cached or not in the 'is_cached' field.
+
+    Note that querying for the token will increase its cache count,
+    making it less likely to be evicted.
+    """
     key = redis_cache.get_cache_key(neighbors, tok)
     (_, in_cache) = redis_cache.check_cache(key)
     return {
         "token": tok,
         "is_cached": True if in_cache is not None else False
     }
+
+@app.get("/neighbors/cache")
+async def neighbors_cache(count:int=100):
+    """
+    Returns a list of tokens in the cache, with up to 'count' entries returned
+    (default 100, max value 1000).
+
+    Note that while these tokens are ordered by the 'freq' field in the list
+    that's returned, they're not retrieved in order of 'freq' due to redis'
+    limitations and the prohibitive cost of querying and returning all the keys
+    to order it ourselves.
+
+    Refer to redis's documentation on the meaning of the 'freq' field in LFU mode:
+    https://redis.io/topics/lru-cache#the-new-lfu-mode
+    """
+    # connect to redis and get the top 100 keys by frequency?
+    r = redis.from_url(os.environ.get("REDIS_URL"))
+
+    # clamp count to something reasonable
+    actual_count = min(count, 1000)
+
+    # determine how the keys are named in redis so we can iterate over them
+    prefix = redis_cache.get_cache_key(neighbors, "").split("tok=")[0]
+
+    # extract the token from the key, e.g. "mouse" from "backend.main.neighbors(mouse)"
+    tok_extract = re.compile(r"%stok=(?P<tok>.*)\)$" % re.escape(prefix))
+
+    # build a list of top "count" tokens, then order it by frequency
+    # (note that if there are more than 'count' tokens, we can't guarantee they're the top ones...)
+    toptokens = [
+        {"token": y, "freq": r.object("freq", x)}
+        for x in islice(r.scan_iter(match=("%s*" % prefix), count=actual_count), actual_count)
+        for y in tok_extract.search(x.decode('utf8')).groups('tok')
+    ]
+
+    return sorted(toptokens, key=lambda x: x['freq'], reverse=True)
