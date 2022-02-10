2b269498f331faeb2e4acba2aba93f3c36cdb0f7
Faisal Alquaddoomi <faisal.alquaddoomi@cuanschutz.edu>
2022-02-10 14:13:33 -0700

    Adds cloudbuild.yaml config, so we can finally persist custom variables (e.g., commit hashes) to gcloud-built images.

--- server/build_deploy.sh
+++ server/build_deploy.sh
@@ -21,12 +21,12 @@ else
 
     (
         [[ "$BUILDS_SKIPPED" == "1" ]] && echo "skipping remote build..." || \
-        gcloud builds submit --project=word-lapse --timeout=2h30m \
-            . --tag gcr.io/word-lapse/word-lapse-api-image:${COMMIT_SHA}
+        gcloud builds submit --config=cloudbuild.yaml \
+            --substitutions=_SHORT_SHA="${SHORT_SHA}",_COMMIT_SHA="${COMMIT_SHA}"
     ) && (
         [[ "$DEPLOY_SKIPPED" == "1" ]] && echo "skipping deploy..." || \
         gcloud compute instances update-container --project=word-lapse word-lapse-api \
-            --container-image gcr.io/word-lapse/word-lapse-api-image:${COMMIT_SHA} \
+            --container-image=gcr.io/word-lapse/word-lapse-api-image:${COMMIT_SHA} \
             --container-mount-host-path=mount-path=/app/data,host-path=/mnt/stateful_partition/word-lapse-data,mode=rw \
             --container-mount-host-path=mount-path=/etc/letsencrypt,host-path=/var/letsencrypt,mode=rw \
             --container-env-file=instance_env
