a757a56caf7ebb8443509cbeea051d8825e80ee4
Faisal Alquaddoomi <faisal.alquaddoomi@cuanschutz.edu>
2022-03-02 11:40:26 -0700

    MIG template now auto-deletes disk (doh), sets instace group to use new template. Fixes some formatting issues.

--- utils/populate_cache/mig_templates/create_mig_template.sh
+++ utils/populate_cache/mig_templates/create_mig_template.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 TEMPLATE_NAME="word-lapse-worker-medium-preempt"
+INSTANCE_GROUP="word-lapse-workers"
 
 PROJECT="word-lapse"
 ZONE="us-central1-a"
@@ -13,7 +14,7 @@ gcloud compute instance-templates create-with-container ${TEMPLATE_NAME} \
     --project=word-lapse \
     --preemptible \
     --machine-type=e2-standard-2 \
-    --create-disk=^,@^description=Stores\ just\ the\ word-lapse\ models,\ without\ version\ control\ metadata.,@device-name=word-lapse-models,@image=projects/word-lapse/global/images/word-lapse-models-02-24-2022,@mode=rw,@size=32,@type=pd-ssd \
+    --create-disk=^,@^description=Stores\ just\ the\ word-lapse\ models,\ without\ version\ control\ metadata.,@device-name=word-lapse-models,@image=projects/word-lapse/global/images/word-lapse-models-02-24-2022,@mode=rw,@size=32,@type=pd-ssd,@auto-delete=yes \
     --container-image=gcr.io/word-lapse/word-lapse-api-image:latest \
     --container-restart-policy=on-failure \
     --container-command=/app/entrypoint_worker.sh \
@@ -24,40 +25,46 @@ gcloud compute instance-templates create-with-container ${TEMPLATE_NAME} \
     --metadata google-logging-enabled=true \
     --metadata-from-file user-data=./cloud-meta
 
+if [[ ${UPDATE_INSTANCE_GROUP:-1} -eq 1 ]]; then
+    echo "Updating instance group ${INSTANCE_GROUP}..."
+    gcloud compute instance-groups managed set-instance-template ${INSTANCE_GROUP} --template=${TEMPLATE_NAME}
+    echo "...done!"
+fi
+
 # test connecting to it
 if [[ ${TEST_CONNECT:-0} -eq 1  ]]; then
     # function for waiting until instance is up
     # courtesy of https://stackoverflow.com/a/65683139
     function wait_vm_up {
-    local counter=0
+        local counter=0
 
-    local readonly instance=${1:?"instance required"}
-    local readonly project=${2:-$PROJECT}
-    local readonly zone=${3:-$ZONE}
-    local readonly user=${4:-$USER}
-    local readonly maxRetry=${5:-100}
+        local readonly instance=${1:?"instance required"}
+        local readonly project=${2:-$PROJECT}
+        local readonly zone=${3:-$ZONE}
+        local readonly user=${4:-$USER}
+        local readonly maxRetry=${5:-100}
 
-    echo "Project: $project"
-    echo "Instance: $instance"
-    echo "MaxRetry: $maxRetry"
+        echo "Project: $project"
+        echo "Instance: $instance"
+        echo "MaxRetry: $maxRetry"
 
-    while true ; do
-        if (( $counter == $maxRetry )) ; then
-        echo "Reach the retry upper limit $counter"
-        exit 1
-        fi
+        while true ; do
+            if (( $counter == $maxRetry )) ; then
+            echo "Reach the retry upper limit $counter"
+            exit 1
+            fi
 
-        gcloud compute ssh --quiet --zone "$zone" "$instance" --tunnel-through-iap --project "$project" --command="true" 2> /dev/null
+            gcloud compute ssh --quiet --zone "$zone" "$instance" --tunnel-through-iap --project "$project" --command="true" 2> /dev/null
 
-        if (( $? == 0 )) ;then
-        echo "The machine is UP !!!"
-        exit 0
-        else
-        echo "Maybe later? $counter"
-        ((counter++))
-        sleep 1
-        fi
-    done
+            if (( $? == 0 )) ;then
+            echo "The machine is UP !!!"
+            exit 0
+            else
+            echo "Maybe later? $counter"
+            ((counter++))
+            sleep 1
+            fi
+        done
     }
 
 
