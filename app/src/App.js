ef9c7a1a8d3be945a9ed27e46714f346a60c0473
Faisal Alquaddoomi <faisal.alquaddoomi@cuanschutz.edu>
2022-04-14 21:24:43 -0600

    Frontend uses CORPORA_SET's values (i.e., labels), submits labels to backend, which can now either take corpus IDs or labels.

--- app/src/App.js
+++ app/src/App.js
@@ -20,7 +20,7 @@ export const AppContext = createContext({});
 const App = () => {
   const [corpus, setCorpus] = useQueryParam(
     "corpus",
-    withDefault(StringParam, (meta?.config?.CORPORA_SET || [])[0] || "")
+    withDefault(StringParam, Object.values(meta?.config?.CORPORA_SET || {})[0] || "")
   );
   const [results, setResults] = useState(null);
   const [search = "", setSearch] = useQueryParam("search", StringParam);
