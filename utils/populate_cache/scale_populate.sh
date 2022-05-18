79a02308943c4940d7b7402e7f5a78696584c909
Faisal Alquaddoomi <faisal.alquaddoomi@cuanschutz.edu>
2022-05-18 14:30:20 -0600

    Hotfix: minor caching populator script updates

--- utils/populate_cache/scale_populate.sh
+++ utils/populate_cache/scale_populate.sh
@@ -1,17 +1,20 @@
 #!/usr/bin/env bash
 
 UTILS_PYTHON="../.venv/bin/python"
+PROJECT="word-lapse"
+ZONE="us-central1-a"
 INSTANCE_GROUP="word-lapse-workers"
+COMMON_ARGS="--project=${PROJECT} ${INSTANCE_GROUP} --zone=${ZONE}"
 
 # nodes to scale up to while we're running
-DESIRED_NODES=32
+DESIRED_NODES=64
 
 # number of parallel jobs to run
 # (less than DESIRED_NODES to maintain some extra capacity for real clients)
-DESIRED_JOBS=31
+DESIRED_JOBS=64
 
 # nodes to keep after we're done
-FINAL_NODES=1
+FINAL_NODES=2
 
 function fail_w_msg {
     echo "$1"
@@ -27,9 +30,9 @@ rm -f non_200s.log
 
 # scale up to the number of nodes we want
 echo "Scaling up nodes to ${DESIRED_NODES}..."
-gcloud compute instance-groups managed resize ${INSTANCE_GROUP}  --project=word-lapse --zone=us-central1-a --size=${DESIRED_NODES} \
+gcloud compute instance-groups managed resize ${COMMON_ARGS} ${INSTANCE_GROUP} --size=${DESIRED_NODES} \
     || fail_w_msg "unable to scale instance groups"
-gcloud compute instance-groups managed wait-until --project=word-lapse --zone=us-central1-a --stable ${INSTANCE_GROUP}
+gcloud compute instance-groups managed wait-until ${COMMON_ARGS} --stable ${INSTANCE_GROUP}
 echo "...done!"
 
 # run the cache populater w/the desired number of jobs
@@ -37,12 +40,12 @@ export SERVER_URL="https://api-wl.greenelab.com"
 export RQ_CONCURRENCY=${DESIRED_JOBS}
 
 export WORD_LIST=${WORD_LIST}
-export CORPUS=${CORPUS:-"abstracts"}
+export CORPUS=${CORPUS:-"pubtator"}
 
 ${UTILS_PYTHON} populate_cache.py
 
 # remove all the instances at the end
 echo "Scaling down to ${FINAL_NODES}..."
-gcloud compute instance-groups managed resize ${INSTANCE_GROUP} --zone=us-central1-a --size=${FINAL_NODES}
-gcloud compute instance-groups managed wait-until --stable ${INSTANCE_GROUP}
+gcloud compute instance-groups managed resize ${COMMON_ARGS} ${INSTANCE_GROUP} --size=${FINAL_NODES}
+gcloud compute instance-groups managed wait-until ${COMMON_ARGS} --stable ${INSTANCE_GROUP}
 echo "...done!"
